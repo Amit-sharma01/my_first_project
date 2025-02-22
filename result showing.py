43b9e88b2a6aa43cb8b9619b46b304e96dcbd1df
Amit Kumar <amit.bscitstudent23.10287@cimage.in>
2025-02-22 20:55:45 +0530

    Add files via upload

---
+++ result showing.py
@@ -0,0 +1,26 @@
+# per=eval(input("enter your percentage:"))
+# if per>=60:
+#     print("first division")
+# elif per>=48:
+#     print("second division")
+# elif per>=35:
+#     print("third division")
+# elif per<=0:
+#     print ("invalid")
+# else:
+#     print ("fail")
+
+# program for eligibility in admission
+
+# marks=eval(input("enter your marks:"))
+# if marks>=300 and marks<=500 :
+#     print("you are considered for admission")
+#     phy=(eval(input("enter physics marks: ")))
+#     che=(eval(input("enter chemistry marks: ")))
+#     math=(eval(input("enter maths marks: ")))
+#     if (int(phy)+int(che)+int(math))>=170:
+#         print ("you are eligible.")
+#     else:
+#         print("you are not eligible.")
+# else:
+#     print("you are not considered for admission")
